e15767394b0a9ee51f9a64ddebd455aef1369d9c
PAVAN903 <navap903@gmail.com>
2023-03-14 15:36:10 +0530

    Added:created queue list

--- src/com/stackandqueue/Main.java
+++ src/com/stackandqueue/Main.java
@@ -1,25 +1,19 @@
 package com.stackandqueue;
 
-import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		Stack<Integer> stack = new Stack<>();
-		stack.push(70);
-		stack.push(30);
-		stack.push(56);
-		System.out.println("peek method in stack");
-		  for (int i = stack.size(); i > 0; i--) {
-			System.out.println("-> " + stack.peek());
-		  }
-			
-			System.out.println("pop method in stack");
-			for(int j=stack.size();j>0;j--) {
-				System.out.println("-> "+stack.pop());
-				
-			}
+		Queue<Integer> que=new LinkedList();
+		que.add(56);
+		que.add(30);
+		que.add(70);
+		while(!que.isEmpty()) {
+			System.out.println(que.poll());
 		}
-
+	}
 	}
 
