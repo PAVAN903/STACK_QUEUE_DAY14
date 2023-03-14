0deb2fd6030e2b21950d9dfe8a12fbfb6984e77f
PAVAN903 <navap903@gmail.com>
2023-03-14 15:25:13 +0530

    Added:peek and pop method added

--- src/com/stackandqueue/Main.java
+++ src/com/stackandqueue/Main.java
@@ -9,10 +9,17 @@ public class Main {
 		stack.push(70);
 		stack.push(30);
 		stack.push(56);
-		for (int i = stack.size(); i > 0; i--) {
-			System.out.println("->" + stack.pop());
+		System.out.println("peek method in stack");
+		  for (int i = stack.size(); i > 0; i--) {
+			System.out.println("-> " + stack.peek());
+		  }
+			
+			System.out.println("pop method in stack");
+			for(int j=stack.size();j>0;j--) {
+				System.out.println("-> "+stack.pop());
+				
+			}
 		}
 
 	}
 
-}
